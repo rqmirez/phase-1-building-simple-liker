4a2ef9b4b21100454f993a2695dea068005d9a9f
Judith Ramirez <rqmirez@gmail.com>
2022-06-21 14:15:09 -0400

    commit tree 2746577ee1a706866b346621cb0408617ce3863b

--- main.js
+++ main.js
@@ -1,10 +1,60 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+  
+const errorModal = document.querySelector("#modal")
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", () =>{
+  console.log("DOM CONTENT HAS LOADED")
+  errorModal.classList.add("hidden")
 
+ // findLikes()
+clickListener()
+
+})
+
+function hideError(){
+  errorModal.classList.add("hidden")
+}
+
+function findLikes(){
+  const likeArr = document.querySelectorAll(".like-glyph")
+
+  likeArr.forEach((singularLike) => {
+    singularLike.addEventListener("click", () => 
+    console.log("YOU FOUND ME! LIKE!"))
+  })
+
+}
+
+function clickListener(){
+  document.addEventListener('click', (event) => {
+
+    if (event.target.classList[0] === "like-glyph"){
+
+      mimicServerCall()
+      .then((response) => {
+        const activated = event.target.classList.contains("activated-heart")
+        if(activated){
+          event.target.classList.remove("activated-heart")
+          event.target.innerHTML = EMPTY_HEART
+        }else{
+          event.target.classList.add("activated-heart")
+          event.target.innerHTML = FULL_HEART
+        }
+        activated
+      })
+      .catch(error => {
+        errorModal.classList.remove("hidden")
+        setTimeout(() => {
+          hideError()
+        }, 3000)
+       })
+  }
+})
+}
 
 
 //------------------------------------------------------------------------------
